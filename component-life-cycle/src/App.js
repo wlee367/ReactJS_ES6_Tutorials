4d8febb625c18539aafe02933de4fea43a58ce7e
Won Ryul Lee <wlee367@uwo.ca>
2018-07-18 18:51:53 -0400

    progress

--- component-life-cycle/src/App.js
+++ component-life-cycle/src/App.js
@@ -8,14 +8,80 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Component Life Cycle</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Body />
       </div>
     );
   }
 }
+
+
+class Body extends Component{
+  constructor(props){
+    super(props);
+
+    this.state = {
+      randomNumber: 0
+    };
+    this.getRandomNumber = this.getRandomNumber.bind(this);
+  }
+
+  getRandomNumber(){
+      this.setState({randomNumber: Math.floor(Math.random()*10) })
+  }
+  render(){
+    return(
+      <div>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+        <button onClick={() => this.getRandomNumber()}>Random Number Generator </button>
+        <Number myNumber={this.state.randomNumber}/>
+      </div>
+    );
+  }
+}
+
+class Number extends Component{
+ 
+  componentDidMount(){
+    console.log("componentDidMount called here");
+  }
+
+  componentWillMount(){
+    console.log('lol');
+  }
+
+  componentWillReceiveProps(newProps){
+    console.log("componentWillReceivePRops called")
+  }
+
+  shouldComponentUpdate(newProps, nextState){
+    console.log('Called should component update');
+    return true;
+  }
+
+  componentWillupdate(newProps, nextState){
+    console.log('Called Component will update');
+  }
+
+  componentDidUpdate(newProps, nextState){
+    console.log('called component did update');
+  }
+
+  componentWillUnmount(){
+    console.log('Called componentwill unmount');
+  }
+  render(){
+    return(
+      <div>
+        <br />
+        {this.props.myNumber}
+      </div>
+    )
+  }
+}
+
 
 export default App;
