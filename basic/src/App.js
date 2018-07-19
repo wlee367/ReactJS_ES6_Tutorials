dcdc7e90fbf31f44eb056098abe484efea2fef9f
Won Ryul Lee <wlee367@uwo.ca>
2018-07-18 20:00:51 -0400

    starting the blog project

---
+++ basic/src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+
+class App extends Component{
+    render(){
+        return(
+            <div>
+                React App from Scratch
+            </div>
+        );
+    }
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
